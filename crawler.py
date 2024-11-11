666cd9a817fecc1c373be94ffaa0880c3b6d5d59
thalita moia <thanalistati@outlook.com>
2024-11-11 09:49:51 +0000

    Adicionando  o método parse para extrair links e nomes dos itens

--- crawler.py
+++ crawler.py
@@ -1,10 +1,25 @@
 import scrapy
 
-
 class CrawlerSpider(scrapy.Spider):
     name = "crawler"
-    
-    start_urls = ["https://itclinical.com/it.php"] # URL inicial
+    start_urls = ["https://itclinical.com/it.php"]
 
+    # extração dos links e nomes
     def parse(self, response):
-        pass
+        SELETOR = ".portfolio-item"  # Seletor CSS para itens de portfólio
+        for categoria in response.css(SELETOR):
+            # link relativo com o link completo
+            link_relativo = categoria.css("a::attr(href)").get()
+            link_completo = response.urljoin(link_relativo)
+
+            # Extraindo o nome do item
+            nome = categoria.css("h5::text").get()
+
+            # dicionário com as informações básicas
+            lista = {
+                'nome': nome,
+                'link': link_completo
+            }
+
+            
+            yield response.follow(link_completo, callback=self.parse_item, meta={'lista': lista})
